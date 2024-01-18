470741b531f50f6aa21d1fd9cc15ea7f872029f7
Christoph Dörrer <d-chris@web.de>
2024-01-18 22:53:47 +0100

    split up tests and use fixture with params

--- tests/test_issue7.py
+++ tests/test_issue7.py
@@ -1,28 +1,61 @@
 import pathlib
+from typing import Any
 
 import pytest
 from pathlibutil import Path, Register7zFormat
 
 
-@pytest.mark.parametrize("cls", [Path, Register7zFormat])
-def test_return_types(tmp_path: pathlib.Path, cls):
+class DummyPath(Register7zFormat):
+    """
+    pathlib.Path > pathlibutil.Path > Register7zFormat > DummyPath
+    """
+
+    pass
+
+
+@pytest.fixture(params=[Path, Register7zFormat, DummyPath])
+def cls(request: pytest.FixtureRequest):
+    return request.param
+
+
+def test_subclass(cls: Any):
+    assert isinstance(cls(__file__), pathlib.Path)
+    assert issubclass(cls, pathlib.Path)
+
+
+def test_copy_type(tmp_path: pathlib.Path, cls: Any):
     p = cls(__file__).copy(tmp_path)
     assert type(p) is cls
     assert type(p) is not pathlib.Path
+    assert isinstance(p, pathlib.Path)
 
+
+def test_context_type(tmp_path: pathlib.Path, cls: Any):
     with cls(tmp_path) as p:
         assert type(p) is cls
         assert type(p) is not pathlib.Path
+        assert isinstance(p, pathlib.Path)
 
+
+def test_archive_type(tmp_path: pathlib.Path, cls: Any):
     p = cls(__file__).make_archive(tmp_path.joinpath("test.zip"))
     assert type(p) is cls
+    assert type(p) is not pathlib.Path
+    assert isinstance(p, pathlib.Path)
 
     z = p.unpack_archive(tmp_path.joinpath("archive"))
     assert type(z) is cls
     assert type(z) is not pathlib.Path
+    assert isinstance(z, pathlib.Path)
 
-    for file in z.iterdir():
-        p = file.move(tmp_path.joinpath("moved"))
-        assert type(p) is cls
-        assert type(p) is not pathlib.Path
-        break
+
+def test_move_type(tmp_path: pathlib.Path, cls: Any):
+    p = cls(__file__).copy(tmp_path)
+
+    dest = tmp_path.joinpath("test")
+    dest.mkdir()
+
+    z = p.move(dest)
+    assert type(z) is cls
+    assert type(z) is not pathlib.Path
+    assert isinstance(z, pathlib.Path)
