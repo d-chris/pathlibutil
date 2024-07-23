591be610d2269e203c7c9fffb87bd69f52d768de
Christoph Dörrer <d-chris@web.de>
2024-07-23 22:42:42 +0200

    added documentation for json

--- pathlibutil/json.py
+++ pathlibutil/json.py
@@ -1,3 +1,19 @@
+"""
+Wrapper around the `json` module that provides a custom JSON encoder for `pathlib.Path`
+objects. This allows `pathlib.Path` objects to be serialized to JSON without having to
+convert them to strings first.
+
+```python
+from pathlib import Path
+from pathlibutil.json import dump
+
+data = {"file": Path(__file__)}
+
+with open("data.json", "w") as f:
+    dump(data, f)
+```
+"""
+
 import functools
 import json
 import pathlib
@@ -5,7 +21,14 @@ from json import load, loads
 
 
 class PathEncoder(json.JSONEncoder):
+    """
+    JSON encoder that converts `pathlib.Path` objects to their string representation.
+    """
+
     def default(self, obj):
+        """
+        Convert `pathlib.Path` objects to a string using `pathlib.Path.as_posix()`.
+        """
         if isinstance(obj, pathlib.Path):
             return obj.as_posix()
 
@@ -22,4 +45,4 @@ def dumps(obj, *, cls=PathEncoder, **kwargs):
     return json.dumps(obj, cls=cls, **kwargs)
 
 
-__all__ = ["load", "loads", "dump", "dumps"]
+__all__ = ["load", "loads", "dump", "dumps", "PathEncoder"]
