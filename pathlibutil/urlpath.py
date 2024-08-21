988621c5ae5d8d0acc2a1dd62824398b7573856c
Christoph Dörrer <d-chris@web.de>
2024-08-21 17:46:27 +0200

    changed netloc to dataclass for modification

--- pathlibutil/urlpath.py
+++ pathlibutil/urlpath.py
@@ -1,7 +1,87 @@
 import pathlib
 import urllib.parse as up
+from copy import copy
+from dataclasses import asdict, dataclass, field
 from functools import wraps
-from typing import Any, Dict, TypeVar, Union
+from typing import Any, Dict, Optional, TypeVar, Union
+
+
+@dataclass(kw_only=True)
+class UrlNetloc:
+    """
+    A dataclass to represent the netloc part of a URL.
+    """
+
+    hostname: str
+    port: Optional[int] = field(default=None)
+    username: Optional[str] = field(default=None)
+    password: Optional[str] = field(default=None)
+
+    def __str__(self) -> str:
+        return self.netloc
+
+    @property
+    def netloc(self) -> str:
+        """return the netloc string"""
+
+        netloc = ""
+
+        if self.username:
+            netloc += self.username
+
+            if self.password:
+                netloc += f":{self.password}"
+
+            netloc += "@"
+
+        netloc += self.hostname
+
+        if self.port:
+            netloc += f":{self.port:d}"
+
+        return netloc
+
+    @classmethod
+    def from_netloc(cls, netloc: str, normalized: bool = False) -> "UrlNetloc":
+        """Parse a netloc string into a `UrlNetloc` object"""
+
+        username = None
+        password = None
+        port = None
+
+        if "@" in netloc:
+            userinfo, netloc = netloc.split("@", 1)
+            if ":" in userinfo:
+                username, password = userinfo.split(":", 1)
+            else:
+                username = userinfo
+
+        if ":" in netloc:
+            hostname, port = netloc.split(":", 1)
+            port = int(port)
+        else:
+            hostname = netloc
+
+        if normalized:
+            hostname = hostname.lower()
+
+        return cls(hostname=hostname, port=port, username=username, password=password)
+
+    def to_dict(self, prune: bool = False) -> Dict[str, Any]:
+        """
+        Convert the `UrlNetloc` object to a dictionary
+
+        If `prune` is `True`, remove all key-value pairs from the dict where the value
+        is `None`.
+        """
+
+        data = asdict(self)
+
+        if not prune:
+            return data
+
+        return {k: v for k, v in data.items() if v is not None}
+
 
 _UrlPath = TypeVar("_UrlPath", bound="UrlPath")
 
@@ -53,7 +133,27 @@ def normalize_url(
     )
 
 
+def urlpath(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return UrlPath(result.geturl(), **result._kwargs)
+
+    return wrapper
+
+
 class UrlPath(up.ParseResult):
+    """
+    Class to manipulate URLs to change the scheme, netloc, path, query, and fragment.
+
+    Wrap the `pathlib.PurePosixPath` methods to return a new `UrlPath` object
+
+    >>> url = UrlPath("https://www.example.com/path/to/file").with_suffix(".txt")
+    >>> str(url)
+    'https://www.example.com/path/to/file.txt'
+
+    """
 
     _default_ports = {
         "http": 80,
@@ -61,7 +161,6 @@ class UrlPath(up.ParseResult):
     }
 
     def __new__(cls, url, **kwargs) -> _UrlPath:
-
         parsed_url = up.urlparse(url, **kwargs)
         return super().__new__(cls, *parsed_url)
 
@@ -71,7 +170,6 @@ class UrlPath(up.ParseResult):
         scheme: str = "",
         allow_fragments: bool = True,
     ) -> None:
-
         self._url = url
         self._kwargs = {
             "scheme": scheme,
@@ -83,12 +181,19 @@ class UrlPath(up.ParseResult):
         return self.geturl(normalize=True)
 
     def geturl(self, normalize: bool = False) -> str:
+        """
+        Return a re-combined version of the URL.
+
+        If `normalize` is `True` schem and netloc is converted  to lowercase,
+        default ports are removed and query parameters are sorted.
+        """
         if normalize:
             return normalize_url(self, ports=self._default_ports, sort=True)
 
         return super().geturl()
 
     def __getattr__(self, attr: str) -> Any:
+
         attr = getattr(self._path, attr)
 
         if not callable(attr):
@@ -98,59 +203,92 @@ class UrlPath(up.ParseResult):
         def wrapper(*args, **kwargs) -> _UrlPath:
             result = attr(*args, **kwargs)
 
-            try:
-                path = result.as_posix()
-            except AttributeError:
-                path = str(result)
-
-            return self.with_path(path)
+            return self.with_path(result)
 
         return wrapper
 
+    @urlpath
     def with_scheme(self, scheme: str) -> _UrlPath:
         return self._replace(scheme=scheme)
 
-    def with_netloc(self, netloc: str) -> _UrlPath:
-        return self._replace(netloc=netloc)
+    @urlpath
+    def with_netloc(self, netloc: Union[str, UrlNetloc]) -> _UrlPath:
+        """
+        Change the netloc of the URL.
+        """
+        return self._replace(netloc=str(netloc))
 
-    def with_path(self, path: str) -> _UrlPath:
+    @urlpath
+    def with_path(self, path: Union[str, pathlib.PurePosixPath]) -> _UrlPath:
+        """
+        Change the path of the URL.
+        """
+
+        try:
+            path = path.as_posix()
+        except AttributeError as e:
+            if not isinstance(path, str):
+                raise TypeError(
+                    f"Expected str or PurePosixPath, got {type(path)}"
+                ) from e
+
         return self._replace(path=path)
 
+    @urlpath
     def with_params(self, params: str) -> _UrlPath:
+        """
+        Change the parameters of the URL.
+        """
         return self._replace(params=params)
 
+    @urlpath
     def with_query(self, query: str) -> _UrlPath:
+        """
+        Change the query of the URL.
+        """
         return self._replace(query=query)
 
+    @urlpath
     def with_fragment(self, fragment: str) -> _UrlPath:
+        """
+        Change the fragment of the URL.
+        """
         return self._replace(fragment=fragment)
 
+    @urlpath
     def with_port(self, port: int) -> _UrlPath:
-        """change the port of the URL or remove it if `port` is `None`"""
+        """
+        change the port in the netloc of the URL.
 
-        netloc = self.netloc.rsplit(":", 1)[0]
+        If `port` is `None`, the port is removed.
+        """
 
-        if port is not None:
-            netloc += f":{port:d}"
+        netloc = UrlNetloc.from_netloc(self.netloc)
+        netloc.port = port
 
         return self.with_netloc(netloc)
 
     def with_hostname(self, hostname: str) -> _UrlPath:
-        """change the hostname of the URL"""
+        """
+        change the hostname in the netloc of the URL
+        """
 
-        netloc = ""
+        netloc = UrlNetloc.from_netloc(self.netloc)
+        netloc.hostname = hostname
 
-        if self.username:
-            netloc = self.username
+        return self.with_netloc(netloc)
 
-            if self.password:
-                netloc += f":{self.password}"
+    def with_credentials(self, username: str, password: str = None) -> _UrlPath:
+        """
+        change the username and password in the netloc of the URL
 
-            netloc += "@"
+        to change only `username` the `password` must also be provided.
 
-        netloc += hostname
+        If `username` is `None`, the credentials are removed.
+        """
 
-        if self.port:
-            netloc += f":{self.port}"
+        netloc = UrlNetloc.from_netloc(self.netloc)
+        netloc.username = username
+        netloc.password = password
 
         return self.with_netloc(netloc)
